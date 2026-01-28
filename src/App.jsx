2085901d391b432c6deeb87cc508d793b0a1aeb0
chamara-kp <kchamara1017@gmail.com>
2026-01-28 20:59:16 +0530

    Initial React frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Collections from "./pages/Collections";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
+import Placeorder from "./pages/Placeorder";
+import Product from "./pages/Product";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placeorder" element={<Placeorder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
