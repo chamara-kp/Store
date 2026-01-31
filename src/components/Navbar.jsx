a953e9d389742284d0ec9af6a85839e1702fb957
chamara-kp <kchamara1017@gmail.com>
2026-01-31 13:02:18 +0530

    Update project files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [Visible, setVisible] = useState(false);
+
   return (
     <div className="flex items-center justify-between py-5 px-6 font-medium">
-      {/* Logo */}
       <img src={assets.logo} alt="Logo" className="w-36" />
 
-      {/* Menu */}
       <ul className="hidden sm:flex gap-8 text-m text-gray-700">
         <li>
           <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -41,29 +42,48 @@ const Navbar = () => {
           </NavLink>
         </li>
       </ul>
+
       <div className="flex items-center gap-6">
-        {/* Search */}
-        <img
-          src={assets.search_icon}
-          className="w-5 cursor-pointer"
-          alt="Search"
-        />
+        <img src={assets.search_icon} className="w-5 cursor-pointer" />
 
-        {/* Profile Dropdown */}
         <div className="relative group">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt="Profile"
-          />
+          <img src={assets.profile_icon} className="w-5 cursor-pointer" />
 
-          {/* Dropdown */}
           <div className="absolute dropdown-menu right-0 top-full mt-2 hidden group-hover:block pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-600 rounded shadow-lg">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
               <p className="cursor-pointer hover:text-black">Logout</p>
             </div>
+          </div>
+        </div>
+
+        <Link to="/" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
+        </Link>
+
+        <img
+          onClick={() => setVisible(true)}
+          src={assets.menu_icon}
+          className="w-5 cursor-pointer sm:hidden"
+        />
+      </div>
+
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          Visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisible(false)}
+            className="flex items-center gap-4 p-3"
+          >
+            <img className="h-4 rotate-180" src={assets.dropdown_icon} />
+            <p>Back</p>
           </div>
         </div>
       </div>
